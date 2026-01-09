6912750c0bad4d5e9949e4d45c002b504f4439bf
Alexandre-legrand281 <pf21tnx@eduvaud.ch>
2026-01-09 13:40:34 +0100

    Update AffTriangleAngleGB.h

--- Exo_7/Exo_7/AffTriangleAngleGB.h
+++ Exo_7/Exo_7/AffTriangleAngleGB.h
@@ -1,18 +1,13 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
-// Néssaire pour l'utilisation du printf_s
+// NÃ©ssaire pour l'utilisation du printf_s
 #include <stdio.h>
 
 // Constantes
-#define MIN_NBR_STARS 1
+#define MIN_NBR_STARS 0
 #define MAX_NBR_STARS 15
 
 #define STAR_CHARACTER "*"
 
-// Prototype de la fonction AffTriangleAngleGB
-unsigned char AffTriangleAngleGB(const int NbrStar);
-
 #endif // END TRIANGLE_H
-
-
