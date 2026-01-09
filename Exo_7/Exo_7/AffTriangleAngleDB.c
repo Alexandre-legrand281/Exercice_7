1dd25d7f71aaa73a6d52819e9a3ca2a3ac4ba742
christophe-Allen <christophe.allenbach@eduvaud.ch>
2026-01-09 13:18:27 +0100

    voila

---
+++ Exo_7/Exo_7/AffTriangleAngleDB.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+// prototype
+char AffTriangleAngleDB(char Nbr_Star);
+
+	// Description
+	//cette fonction affiche un triangle en bas à gauche
+    // et compte le nombre d'étoile total du triangle
+    // 
+	// auteur : Allenbach Christophe et Espinosa Dilan 
+	char AffTriangleAngleDB(char Nbr_Star)
+{
+    char i;
+    char j;
+    char total = 0;
+    char etoile = '*';
+    char vide = ' ';
+
+    for (i = 1; i <= Nbr_Star; i++)
+    {
+        // zone de vide
+        for (j = 1; j <= Nbr_Star - i; j++)
+        {
+            printf("%c", vide);
+        }
+
+        // affichage des Étoiles
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", etoile);
+            total++;
+        }
+        printf("\n");
+    }
+    return(total);
+}
