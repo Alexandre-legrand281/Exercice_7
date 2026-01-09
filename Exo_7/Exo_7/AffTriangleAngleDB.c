b5ee9815c0eb37ebf8c21a3e065d5f865bcaef92
christophe-Allen <christophe.allenbach@eduvaud.ch>
2026-01-09 14:20:29 +0100

    Update AffTriangleAngleDB.c

--- Exo_7/Exo_7/AffTriangleAngleDB.c
+++ Exo_7/Exo_7/AffTriangleAngleDB.c
@@ -1,21 +1,36 @@
+//-----------------------------------------------------------------------------------//
+// Nom du projet 		: Exercice7
+// Nom du fichier 		: main_Exo7.c
+// Date de création 	: 08.12.2025
+// Date de modification : 08.01.2026
+//
+// Auteur 				: CAH DEA
+//
+// Description          : cette fonction affiche un triangle en bas à gauche
+// 						  et compte le nombre d'étoile total du triangle
+//
+//
+// Remarques :          :  -
+//----------------------------------------------------------------------------------//
+
 #include <stdio.h>
 #include <math.h>
 #include "EXO7.h"
 
-// prototype
-char AffTriangleAngleDB(char Nbr_Star);
-
-	// Description
-	//cette fonction affiche un triangle en bas à gauche
-    // et compte le nombre d'étoile total du triangle
-    // 
-	// auteur : Allenbach Christophe et Espinosa Dilan 
-	char AffTriangleAngleDB(char Nbr_Star)
+//----------------------------------------------------------------------------------//
+//-- nom fct : CalculPerimetreCercle
+//-- paramètre entrée : Nbr_Star (char)
+//-- paramètre sortie : total (char)
+//-- paramètre IN-OUT : - 
+//-- description	  : faire un triangle avec un nombre d'étoile donnée et de calculer le nombre d'étoile total du triangle
+//----------------------------------------------------------------------------------//
+	
+char AffTriangleAngleDB(char Nbr_Star)
 {
     char i;
     char j;
-    char total = 0;
-    char etoile = '*';
+    char totalStars = 0;
+    char Star = '*';
     char vide = ' ';
 
     for (i = 1; i <= Nbr_Star; i++)
@@ -29,11 +44,12 @@ char AffTriangleAngleDB(char Nbr_Star);
         // affichage des Étoiles
         for (j = 1; j <= i; j++)
         {
-            printf("%c", etoile);
-            total++;
+            printf("%c", Star);
+            totalStars++;
         }
         printf("\n");
     }
-    return(total);
+    return(totalStars);
 }
+
 
